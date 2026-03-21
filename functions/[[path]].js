fd829bd4c8f17e47af4054c629945daba34dfb28
Yosshy <Yosshy_123@proton.me>
2026-03-22 06:51:52 +0900

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,17 +1,25 @@
 export async function onRequest(context) {
-  try {
-    const url = new URL(context.request.url);
+  const { request, env } = context
+  const url = new URL(request.url)
 
-    const response = await context.env.ASSETS.fetch(url.pathname);
-
-    if (response.status === 404) {
-      const index = await context.env.ASSETS.fetch('/index.html');
-      return new Response(index.body, { status: 404, headers: index.headers });
-    }
-
-    return response;
-  } catch (err) {
-    console.error('worker error', err);
-    return new Response('Internal Error', { status: 500 });
+  const assetResponse = await env.ASSETS.fetch(request)
+  if (assetResponse.status !== 404) {
+    return assetResponse
   }
+
+  return await serveNotFoundPage(env, url)
+}
+
+async function serveNotFoundPage(env, url) {
+  const indexUrl = new URL("/index.html", url)
+  const indexResponse = await env.ASSETS.fetch(indexUrl)
+
+  const headers = new Headers(indexResponse.headers)
+  headers.set("Content-Type", "text/html; charset=UTF-8")
+
+  return new Response(indexResponse.body, {
+    status: 404,
+    statusText: "Not Found",
+    headers,
+  })
 }
