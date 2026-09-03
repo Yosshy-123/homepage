d10f319ccaf9d7feedbb44593f7b4f2af0f56b82
Yosshy <Yosshy_123@proton.me>
2026-09-03 18:47:54 +0900

    aaa

--- .github/README.md
+++
@@ -1,5 +0,0 @@
-# HomePage
-
-[https://yosshy.f5.si/](https://yosshy.f5.si/)
-
-[![Built with Cloudflare](https://workers.cloudflare.com/built-with-cloudflare.svg)](https://cloudflare.com)
--- src/data/projects.ts
+++
@@ -1,9 +0,0 @@
-import type { Project } from '../types/project'
-
-export const projects: readonly Project[] = [
-  {
-    title: 'KAeRU Log',
-    description: 'Lightweight real-time chat powered by Redis',
-    href: 'https://kaeru-log.yosshy.f5.si/',
-  },
-] as const
